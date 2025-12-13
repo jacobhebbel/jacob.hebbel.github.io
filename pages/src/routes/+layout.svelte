a7f19f852a4943e17371a5492618680e1323e4b1
jaocbhebbel <jacob.hebbel@gmail.com>
2025-12-13 13:39:15 -0500

    framework and root

---
+++ pages/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</svelte:head>
+
+{@render children()}
